bf259ce06aec71c2c32ff18612c6621aa83b1299
Andrew Marquez <andy2mgcc@gmail.com>
2018-10-18 22:09:00 -0600

    Made API call and jQuery

--- script.js
+++ script.js
@@ -27,15 +27,29 @@ function mainCtrl ($scope) {
     $scope.toTrack = ['GOOGL', 'AAPL']
     $scope.stocks = [];
 
-    $scope.addStockPrice = function (s) {
-        s.symbol = "GOOGL";
-        s.date = "10/18/18";
-        s.open = Math.floor(Math.random() * 100) + 100;
-        s.close = Math.floor(Math.random() * 100) + 100;
-        $scope.stocks.push(s);
+    $scope.addStockPrice = function (dp) {
+        $scope.stocks.push(dp);
+        console.log($scope.stocks);
     }
 
-    for (var i = 0; i < 50; i++) {
-        $scope.addStockPrice(new StockDayPrice);
+    for (let ticker of $scope.toTrack) {
+        five_year_url = 'https://api.iextrading.com/1.0/stock/' + ticker + '/chart/5y';
+        $.getJSON(five_year_url)
+            .done((response) => {
+                var i = 0;
+                for (let price of response) {
+                    if (i > 10) {
+                        console.log(i);
+                        break;
+                    }
+                    let dayprice = new StockDayPrice(
+                        ticker, price.date, price.open, price.close,
+                        price.high, price.low, price.change, price.changePercent
+                    );
+                    $scope.addStockPrice(dayprice);
+                    i++;
+                }
+            })
+            .fail(() => console.log("Trouble grabbing data for " + ticker));
     }
 }
