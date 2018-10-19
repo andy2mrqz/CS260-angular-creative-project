f47b3daee3b11b59f493f4af77cac8a97ca65c2f
Andrew Marquez <andy2mgcc@gmail.com>
2018-10-18 21:14:55 -0600

    Created classes for the stock objects

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+// Class definitions
+class StockDayPrice {
+    constructor(symbol, date, open, close, high, low, change, changePercent) {
+        this.symbol = symbol;
+        this.date = date;
+        this.open = open;
+        this.close = close;
+        this.high = high;
+        this.low = low;
+        this.change = change;
+        this.changePercent = changePercent;
+    }
+}
+
+class StockMinutePrice {
+    constructor(symbol, datetime, price) {
+        this.symbol = symbol;
+        this.datetime = datetime;
+        this.price = price;
+    }
+}
+
 angular.module('app', [])
     .controller('mainCtrl', mainCtrl);
 
