dadf66e129454513b61471f307efeb563328f4ac
Andrew Marquez <andy2mgcc@gmail.com>
2018-10-18 22:49:20 -0600

    Added function to refresh stock values

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ angular.module('app', ['n3-line-chart'])
     .controller('mainCtrl', mainCtrl);
 
 function mainCtrl ($scope) {
-    $scope.toTrack = ['GOOGL']
+    $scope.toTrack = []
     $scope.stocks = [];
 
     $scope.data = {
@@ -51,30 +51,35 @@ function mainCtrl ($scope) {
     $scope.addNew = function(stock) {
         $scope.toTrack.push(stock.ticker);
         stock.ticker = "";
+        $scope.getStockData();
     }
 
     $scope.clear = function() {
         $scope.toTrack = [];
+        $scope.getStockData();
     }
 
     $scope.addStockPrice = function (dp) {
         $scope.stocks.push(dp);
     }
 
-    // Get 5 year stock data for each stock we want to track and add to stocks
-    for (let ticker of $scope.toTrack) {
-        five_year_url = 'https://api.iextrading.com/1.0/stock/' + ticker + '/chart/5y';
-        $.getJSON(five_year_url)
-            .done((response) => {
-                for (let price of response) {
-                    let dayprice = new StockDayPrice(
-                        ticker, price.date, price.open, price.close,
-                        price.high, price.low, price.change, price.changePercent
-                    );
-                    $scope.addStockPrice(dayprice);
-                }
-                $scope.$apply();
-            })
-            .fail(() => console.log("Trouble grabbing data for " + ticker));
+    $scope.getStockData = function () {
+        // Get 5 year stock data for each stock we want to track and add to stocks
+        for (let ticker of $scope.toTrack) {
+            five_year_url = 'https://api.iextrading.com/1.0/stock/' + ticker + '/chart/5y';
+            $.getJSON(five_year_url)
+                .done((response) => {
+                    for (let price of response) {
+                        let dayprice = new StockDayPrice(
+                            ticker, price.date, price.open, price.close,
+                            price.high, price.low, price.change, price.changePercent
+                        );
+                        $scope.addStockPrice(dayprice);
+                    }
+                    $scope.$apply();
+                })
+                .fail(() => console.log("Trouble grabbing data for " + ticker));
+        }
     }
+
 }
